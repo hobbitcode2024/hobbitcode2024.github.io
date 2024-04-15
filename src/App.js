492949e18f9fa1f2092a8acff32dbebabce58931
hobbitcode2024 <hobbitcode2024@gmail.com>
2024-04-14 22:20:01 -0500

    fix path

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 
 import PrivacyPolicy from './app/PrivacyPolicy.js';
 import TermsOfService from './app/TermsOfService.js';
 import Home from './app/Home.js';
 
+const Router = process.env.NODE_ENV === 'development' ? BrowserRouter : HashRouter;
+
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/privacy" element={<PrivacyPolicy />} />
-        <Route exact path="/terms" element={<TermsOfService />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsOfService />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
